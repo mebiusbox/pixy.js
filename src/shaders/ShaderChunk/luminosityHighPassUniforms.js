89377365d88f5e17366c1cbd9169a1f305be50c9
mebiusbox <flare.mebius@gmail.com>
2017-03-11 17:43:57 +0900

    add ShaderChunk: luminosityHighPass

---
+++ src/shaders/ShaderChunk/luminosityHighPassUniforms.js
@@ -0,0 +1,7 @@
+export var luminosityHighPassUniforms = {
+  tDiffuse: { value: null },
+  luminosityThreshold: { value: 1.0 },
+  smoothWidth: { value: 1.0 },
+  defaultColor: { value: new THREE.Color(0x000000) },
+  defaultOpacity: { value: 0.0 }
+};
