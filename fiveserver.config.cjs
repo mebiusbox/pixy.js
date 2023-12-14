e3b7ac2526b69a6f9f8b71decd31c857d0373010
mebiusbox <flare.mebius@gmail.com>
2023-12-14 18:16:55 +0900

    use five-server instead of servez

---
+++ fiveserver.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+	port: 8086,
+	// root: 'samples',
+	open: ['samples/'],
+	// host: '0.0.0.0',
+	// browser: 'chrome',
+	// https: true,
+	watch: [ 'samples' ],
+	ignore: [ /\.s[ac]ss$/i ]
+};
